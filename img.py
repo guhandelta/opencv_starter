93577453de9754016a5cc21e7f02a6ab37df4d6e
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-12-07 14:30:49 -0500

    Resized the image with the resize function from opencv library

--- img.py
+++ img.py
@@ -1,8 +1,12 @@
 import cv2
 
 img = cv2.imread("ac4.jpg",0) # 1 - read img in color || 0 - read img n b/w || -1 - read img in color, with transparency
+rs = cv2.resize(img,(int(img.shape[0]/2),int(img.shape[1]/2))) # img.shap[0] => height || img.shape[1] => width
+rs1 = cv2.resize(img,(int(img.shape[0]/0.5),int(img.shape[1]/3)))
 print(img)
 print(type(img))
+cv2.imwrite("resize.jpg",rs)
+cv2.imwrite("resize1.jpg",rs1)
 cv2.imshow("show",img)
 cv2.waitKey(5000)
 cv2.destroyAllWindows()
